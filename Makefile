7ea9ac77da78beeec12754da96f092a532392c69
proof <lichangkuan9@gmail.com>
2025-04-07 20:04:35 +0800

    克隆

--- Makefile
+++ Makefile
@@ -1,6 +1,14 @@
+CC := gcc
+
 log :=  thirdparty/log/log.h thirdparty/log/log.c
 log_test: test/log_test.c $(log)
-	-gcc $^ -o $@ -I thirdparty
+	-$(CC) $^ -o $@ -I thirdparty
+	-./$@
+	-rm $@
+
+json := thirdparty/cJSON/cJSON.h thirdparty/cJSON/cJSON.c
+json_test: test/json_test.c $(json) $(log)
+	-$(CC) $^ -o $@ -I thirdparty
 	-./$@
 	-rm $@
 
--- test/json_test.c
+++ test/json_test.c
@@ -1,10 +1,29 @@
 #include "cJSON/cJSON.h"
-
+#include "log/log.h"
+#include "stdlib.h"
 int main(int argc, char *argv[])
 {
     // 生成json字符串
     cJSON *root_obj = cJSON_CreateObject();
-    
+    cJSON_AddStringToObject(root_obj, "name", "test");
+    cJSON_AddNumberToObject(root_obj, "age", 18);
+    char *json = cJSON_PrintUnformatted(root_obj);
+    log_debug("生成json字符串: %s", json);
 
+    // 解析json字符串
+    cJSON *root_obj2 = cJSON_Parse(json);
+    if (root_obj2 == NULL) {
+        log_error("解析json字符串失败");
+        return -1;
+    }
+    char *name = cJSON_GetObjectItem(root_obj2, "name")->valuestring;
+    int age = cJSON_GetObjectItem(root_obj2, "age")->valueint;
+    log_debug("解析json字符串: name=%s, age=%d", name, age);
+
+    // 释放内存
+    cJSON_Delete(root_obj);
+    cJSON_Delete(root_obj2);
+    free(json);
+    
     return 0;
 }
